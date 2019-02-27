2923d9296bbe7fe91b0cd472f141e3f97692e3b9
Theodore Olanipekun Akinyede <Olanipekun.Akinyede@UCDConnect.ie>
2019-02-27 20:09:34 +0000

    more pre-merge changes

--- webApp/src/webApp/beans/Room.java
+++ webApp/src/webApp/beans/Room.java
@@ -1,6 +1,8 @@
 package webApp.beans;
 
 import java.io.Serializable;
+import java.text.DecimalFormat;
+
 
 // A class for hotel rooms
 public class Room implements Serializable{
@@ -8,12 +10,18 @@ public class Room implements Serializable{
 	// Fields
 	private static final long serialVersionUID = 1L;
 	private String roomNumber;
+	private int capacity;
 	private int reservationNumber;
+	private double price;
+    String pattern="\u20ac###,###.##";
+    DecimalFormat euroFormatter = new DecimalFormat(pattern);
+	private String priceFormatted;
 	
 	// Constructor
 	public Room(String Room_Number) {
 		super();
 		this.roomNumber = Room_Number;
+		
 	}
 	// Getters and Setters
 	public String getRoomNumber() {
@@ -28,6 +36,25 @@ public class Room implements Serializable{
 	}
 	public void setReservationNumber(int reservationNumber) {
 		this.reservationNumber = reservationNumber;
+	}
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+		this.priceFormatted = euroFormatter.format(price);
+	}
+	public String getPriceFormatted() {
+		return priceFormatted;
+	}
+	public void setPriceFormatted(String priceFormatted) {
+		this.priceFormatted = priceFormatted;
 	}
 
 	
--- webApp/src/webApp/servlets/ReservationChooseRoom.java
+++ webApp/src/webApp/servlets/ReservationChooseRoom.java
@@ -3,6 +3,7 @@ package webApp.servlets;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
@@ -24,80 +25,78 @@ import webApp.dbconn.DB_rooms;
 
 @WebServlet(urlPatterns = { "/ReservationChooseRoom" })
 public class ReservationChooseRoom extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
+    
+ 
+    public ReservationChooseRoom() {
+        super();
+    }
+ 
+    
+    // doGet
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    	
+    	
+    	// Connect to database
+        Connection conn = SessionUtils.getStoredConnection(request);
+        
+        
+    	HttpSession session = request.getSession();
+    	
+    	String resStart = request.getParameter("resStart");
+    	String resEnd = request.getParameter("resEnd");
+    	String rooms = request.getParameter("numRooms");
+    	
+    	
 
-
-	public ReservationChooseRoom() {
-		super();
-	}
-
-
-	// doGet
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-
-		//Â Connect to database
-		Connection conn = SessionUtils.getStoredConnection(request);
-
-
-		HttpSession session = request.getSession();
-
-		String resStart = request.getParameter("resStart");
-		String resEnd = request.getParameter("resEnd");
-		String rooms = request.getParameter("numRooms");
-
-
-
-		DateTimeFormatter f = DateTimeFormatter.ofPattern( "yyyy-MM-dd" ) ;
-		LocalDate start = LocalDate.parse( resStart , f ) ;
-		LocalDate end = LocalDate.parse( resEnd , f ) ;
-		Long durationLong = ChronoUnit.DAYS.between(start, end);
-		int duration = durationLong.intValue();
-		int numRooms = Integer.parseInt(rooms);
-
-		// format LocalDate to display on webpage
-		DateTimeFormatter formatWeb = DateTimeFormatter.ofPattern("EEEE, dd MMMM, yyyy");
-
-		System.out.println("Hello " + start + " " + end + " " + duration);
-
-		// set object attributes to add to session
-		session.setAttribute("startObj", start);
-		session.setAttribute("endObj", end);
-
-
-		// set String/int attributes to send to web page
-		session.setAttribute("startDate", formatWeb.format(start));
-		session.setAttribute("endDate", formatWeb.format(end));
-		session.setAttribute("numRooms", numRooms);
-		session.setAttribute("duration", duration);
-
-
-
-
-		// Find number of total rooms
-		try {
-
-			System.out.println("WHO??Â ");
-			int totalRooms = DB_rooms.countTotalRooms(conn);
-			System.out.println("Rooms:   "+ totalRooms);		 
+    	DateTimeFormatter f = DateTimeFormatter.ofPattern( "yyyy-MM-dd" ) ;
+    	LocalDate start = LocalDate.parse( resStart , f ) ;
+    	LocalDate end = LocalDate.parse( resEnd , f ) ;
+    	Long durationLong = ChronoUnit.DAYS.between(start, end);
+    	int duration = durationLong.intValue();
+    	int numRooms = Integer.parseInt(rooms);
+    		
+    	// format LocalDate to display on webpage
+    	DateTimeFormatter formatWeb = DateTimeFormatter.ofPattern("EEEE, dd MMMM, yyyy");
+    		
+    	System.out.println("Hello " + start + " " + end + " " + duration);
+    		
+    	// set object attributes to add to session
+    	session.setAttribute("startObj", start);
+    	session.setAttribute("endObj", end);
+    		
+    		
+    	// set String/int attributes to send to web page
+    	session.setAttribute("startDate", formatWeb.format(start));
+    	session.setAttribute("endDate", formatWeb.format(end));
+    	session.setAttribute("numRooms", numRooms);
+    	session.setAttribute("duration", duration);
+    	
+    		
+    		
+    		
+    	// Find number of total rooms
+    	try {
+    			
+			int totalRooms = DB_rooms.countTotalRooms(conn);	 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-
-
-
-		// Retrieve a list of all reservations
+	        
+    		
+    	
+    	
+    	// Retrieve a list of all reservations
 		List<Reservation> allReservations = null;
 		List<Room> availableRooms = null;
 		int numberAvailableRooms = 0;
-
+		
 		try {
 			allReservations = DB_reservation.queryAllReservations(conn);
-			// Retrieve a list of rooms
+			 // Retrieve a list of rooms
 			//availableRooms = DB_rooms.selectRooms(conn);
 			availableRooms = DB_rooms.selectAvailableRooms(conn, start, end);
 			numberAvailableRooms = availableRooms.size();
@@ -105,110 +104,118 @@ public class ReservationChooseRoom extends HttpServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		
 		if (allReservations != null & availableRooms != null) {
-			System.out.println("YES!"+ allReservations.toString());
-			// set attribute for Room objects so they are accessible by the ReservationChooseRoom.jsp page
-			session.setAttribute("availableRooms", availableRooms);
-			session.setAttribute("numberAvailableRooms", numberAvailableRooms);
-
-
+		 System.out.println("YES!"+ allReservations.toString());
+		 // set attribute for Room objects so they are accessible by the ReservationChooseRoom.jsp page
+		 session.setAttribute("availableRooms", availableRooms);
+		 session.setAttribute("numberAvailableRooms", numberAvailableRooms);
+		 
+		
 		}
-
+		
 		// Load the choose room view
-		RequestDispatcher dispatcher = request.getServletContext()
+        RequestDispatcher dispatcher = request.getServletContext()
+        		
+                .getRequestDispatcher("/WEB-INF/views/ReservationChooseRoom.jsp");
+        dispatcher.forward(request, response);
+        
+    }
+ 
 
-				.getRequestDispatcher("/WEB-INF/views/ReservationChooseRoom.jsp");
-		dispatcher.forward(request, response);
+    // doPost is a Servlet post method handler
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    	
+	    // Request the current session and use it to retrieve objects from Reservation step 1
+	    HttpSession session = request.getSession();
+	    LocalDate resStart = (LocalDate) session.getAttribute("startObj");
+	    LocalDate resEnd = (LocalDate) session.getAttribute("endObj");
+	    int duration = (int) session.getAttribute("duration");
+	    int numRooms = (int) session.getAttribute("numRooms");
 
-	}
+	    // Get an array of chosen Hotel rooms
+	    String[] choices = null;
+	    choices = request.getParameterValues("choices");
+	    
+	  // Connect to database
+        Connection conn = SessionUtils.getStoredConnection(request);
+	    
+        double resPrice = 0;
+       
+	    
+	    // Room choice form validation
+	    if(choices.length!= numRooms) {
+	    	session.setAttribute("validationCount", "Hey, that's not the number of rooms you requested! Try again.");
 
-
-	// doPost is a Servlet post method handler
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-		// Request the current session and use it to retrieve objects from Reservation step 1
-		HttpSession session = request.getSession();
-		LocalDate resStart = (LocalDate) session.getAttribute("startObj");
-		LocalDate resEnd = (LocalDate) session.getAttribute("endObj");
-		int duration = (int) session.getAttribute("duration");
-		int numRooms = (int) session.getAttribute("numRooms");
-
-		// Get an array of chosen Hotel rooms
-		String[] choices = null;
-		choices = request.getParameterValues("choices");
-
-		// Room choice form validation
-		if(choices.length!= numRooms) {
-			System.out.println("dingus");
-			session.setAttribute("validationCount", "Hey, that's not the number of rooms you requested! Try again.");
-
-
+	    	
 			// Load reservationBookingView
-			RequestDispatcher dispatcher = request.getServletContext()
+	        RequestDispatcher dispatcher = request.getServletContext()
+	        		
+	                .getRequestDispatcher("/WEB-INF/views/ReservationChooseRoom.jsp");
+	        dispatcher.forward(request, response);
+	        }
+	    
+	    else {
+	    	
+	    	session.setAttribute("validationCount", "");
 
-					.getRequestDispatcher("/WEB-INF/views/ReservationChooseRoom.jsp");
-			dispatcher.forward(request, response);
-		}
-
-		else {
-
-			session.setAttribute("validationCount", "");
-
-			//DateTimeFormatter f = DateTimeFormatter.ofPattern( "yyyy-MM-dd" ) ;
-
-			//Â Connect to database
-			Connection conn = SessionUtils.getStoredConnection(request);
+			DateTimeFormatter f = DateTimeFormatter.ofPattern( "yyyy-MM-dd" ) ;
+	
 
 			// Retrieve the booked rooms from the database
 			try {
-				int totalRooms = DB_rooms.countTotalRooms(conn);
-				System.out.println("Rooms:   "+ totalRooms);
-
-
+				 int totalRooms = DB_rooms.countTotalRooms(conn);
+				 System.out.println("Rooms:   "+ totalRooms);
+				 
+				 // get roomnumber from choices then run query to get room number's price
+				 
+			        for (int i=0; i<choices.length; i++) {
+			        	String choice=choices[i];
+			        	Double price = DB_rooms.selectRoomPrice(conn, choice);
+			        	resPrice += price;
+			        	}
+			        
+			        resPrice = resPrice * duration; 
+				 
+			
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
+			
 			// format LocalDate to display on webpage
 			DateTimeFormatter formatWeb = DateTimeFormatter.ofPattern("EEEE, dd MMMM, yyyy");
-
+			
 			System.out.println("Hello " + resStart + " " + resStart + " " + duration);
-
+			
 			// set object attributes to send to confirmation servlet
 			session.setAttribute("startObj", resStart);
 			session.setAttribute("endObj", resEnd);
-
+			
 			// set String/int attributes to send to web page
 			session.setAttribute("startDate", formatWeb.format(resStart));
 			session.setAttribute("endDate", formatWeb.format(resEnd));
 			session.setAttribute("numRooms", numRooms);
 			session.setAttribute("duration", duration);
 			session.setAttribute("choices", choices);
+		    String pattern="\u20ac###,###.##";
+		    DecimalFormat euroFormatter = new DecimalFormat(pattern);
+			session.setAttribute("reservationPrice", euroFormatter.format(resPrice));
+			session.setAttribute("resPrice", resPrice);
+			
+	        
 
 
-			if(SessionUtils.getLoginedUser(request.getSession())==null)
-			{
-				RequestDispatcher dispatcher = request.getServletContext()
+			// Load reservationBookingView
+	        RequestDispatcher dispatcher = request.getServletContext()
+	        		
+	                .getRequestDispatcher("/WEB-INF/views/reservationBookingView.jsp");
+	        dispatcher.forward(request, response);
+	    
+	    }
+	    
 
-						.getRequestDispatcher("/WEB-INF/views/reservationTwoView.jsp");
-				dispatcher.forward(request, response);
-
-
-			}
-			else
-			{
-				RequestDispatcher dispatcher = request.getServletContext()
-
-						.getRequestDispatcher("/WEB-INF/views/starwoodChooseRoom.jsp");
-				dispatcher.forward(request, response);
-
-			}
-
-
-		}
 	}
 }
